936b58bd8cf24b752450d9120d48672d6df264f6
seonhyeokjun <tjsrmatn@naver.com>
2024-11-08 00:26:51 +0900

    Toss Payments 결제 연동

---
+++ src/main/kotlin/com/example/paymentservice/payment/adapter/in/web/api/TossPaymentController.kt
@@ -0,0 +1,31 @@
+package com.example.paymentservice.payment.adapter.`in`.web.api
+
+import com.example.paymentservice.common.WebAdapter
+import com.example.paymentservice.payment.adapter.`in`.web.request.TossPaymentConfirmRequest
+import com.example.paymentservice.payment.adapter.`in`.web.response.ApiResponse
+import com.example.paymentservice.payment.adapter.out.web.executor.TossPaymentExecutor
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+
+@WebAdapter
+@RequestMapping("/v1/toss")
+@RestController
+class TossPaymentController (
+    private val tossPaymentExecutor: TossPaymentExecutor
+) {
+    @PostMapping("/confirm")
+    fun confirm(@RequestBody request: TossPaymentConfirmRequest) : Mono<ResponseEntity<ApiResponse<String>>> {
+        return tossPaymentExecutor.execute(
+            request.paymentKey, request.orderId, request.amount.toString()
+        ).map {
+            ResponseEntity.ok().body(
+                ApiResponse.with(HttpStatus.OK, "OK", it)
+            )
+        }
+    }
+}
